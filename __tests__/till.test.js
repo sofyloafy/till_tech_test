ca372cc04a52da5f0acad16836e8fef071d2a60a
Sophie <sophiebrown438@gmail.com>
2020-08-17 17:49:16 +0100

    Pass test for header

--- __tests__/till.test.js
+++ __tests__/till.test.js
@@ -3,6 +3,14 @@ let till = new Till
 
 describe('#Till', () => {
   test('it should return first array item', () => {
-    expect(till).toBe()
+    expect(till.calculateReceipt("Affogato")).toBe(14.80)
+  })
+
+  test('it should return first array item', () => {
+    expect(till.calculateReceipt("Cappucino")).toBe(3.85)
+  })
+
+  test('it should return first array item', () => {
+    expect(till.header()).toEqual("The Coffee Connection\n123 Lakeside Way")
   })
 })
--- src/till.js
+++ src/till.js
@@ -1,4 +1,22 @@
+let coffeeData = require('../hipstercoffee.json')
+
+
 class Till {
+  constructor() {
+    this.total = 0
+    this.receipt = ""
+  }
+
+  calculateReceipt(item) {
+    this.total += coffeeData[0].prices[0][item]
+    return coffeeData[0].prices[0][item]
+  }
+
+  header(){
+    this.receipt += (coffeeData[0].shopName + "\n")
+    this.receipt += coffeeData[0].address
+    return this.receipt
+  }
 
 }
 module.exports = Till 
